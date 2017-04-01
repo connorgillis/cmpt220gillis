53bdd7400e2c404a6db417326da8281074f4a39e
Juan Arias <juan.arias@marist.edu>
2017-04-01 19:53:44 -0400

    Feedback on Lab 8

--- labs/8/Problem11dot13.java
+++ labs/8/Problem11dot13.java
@@ -30,6 +30,7 @@ public class Problem11dot13 {
     }
 
     //remove duplicates methods, takes ArrayList obj
+	// JA: A better approach is to replicate the arraylist without the duplicates.
     public static void removeDuplicate(ArrayList<Integer> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = i + 1; j < list.size(); j++) {
